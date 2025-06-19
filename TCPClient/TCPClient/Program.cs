8bab36382cc1801a61ce05871c53e122c494ae57
henry2craftman <henry2craftman@gmail.com>
2025-06-19 16:42:31 +0900

    feat: Server, Client 1:1접속 예제 완료

--- TCPClient/TCPClient/Program.cs
+++ TCPClient/TCPClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Sockets;
+﻿using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPClient
 {
@@ -9,7 +11,24 @@ namespace TCPClient
             // 서버에 연결
             TcpClient client = new TcpClient("127.0.0.1", 12345); // 로컬아이피, 포트 
 
-            Console.Write("서버에 연결됨.");
+            Console.Write("서버에 연결됨.\n");
+
+            // 1. 네트워크 스트림 얻기
+            NetworkStream stream = client.GetStream();
+
+            while(true)
+            {
+                // 2. 서버에 데이터 보내기
+                string data = Console.ReadLine();
+                byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+                stream.Write(dataBytes, 0, dataBytes.Length);
+
+                // 3. 서버로 부터 데이터 읽기
+                byte[] buffer = new byte[1024];
+                int byteNumber = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, byteNumber);
+                Console.WriteLine(response);
+            }
         }
     }
 }
--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace TCPServer
 {
@@ -14,12 +15,28 @@ namespace TCPServer
 
             Console.WriteLine("서버 시작");
 
+            // 1. 클라이언트의 연결을 대기
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("클라이언트 연결됨.\n");
+
+            // 2. 네트워크의 스트림 가져오기
+            NetworkStream stream = client.GetStream();
+
             while(true)
             {
-                // 클라이언트의 연결을 대기
-                TcpClient client = server.AcceptTcpClient();
+                // 3. 클라이언트로 부터 데이터 읽기
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
 
-                Console.WriteLine("클라이언트 연결됨");
+                // 4. 버퍼에 저장한 데이터를 인코딩(UTF8)
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("클라이언트: " + data);
+
+                // 5. 답장 쓰기(데이터 전송하기)
+                string response = "서버: 데이터 받음\n";
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseData, 0, responseData.Length);
             }
         }
     }
