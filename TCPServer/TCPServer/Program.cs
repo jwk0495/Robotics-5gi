3854193ff435d8565820034e5d81fd48cd28c9bf
henry2craftman <henry2craftman@gmail.com>
2025-06-20 15:28:02 +0900

    feat: MxComponent Server 프로그램 완성

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using ActUtlType64Lib;
 
 namespace TCPServer
 {
     class Program
     {
+        enum State
+        {
+            CONNECTED,
+            DISCONNECTED
+        }
+        static ActUtlType64 mxComponent;
+        static State state;
+
         static async Task Main()
         {
+            // 콘솔 프로그램 종료 이벤트 등록
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ExitHandler);
+
+            // MxComponent 초기설정
+            mxComponent = new ActUtlType64();
+            mxComponent.ActLogicalStationNumber = 1;
+            state = State.DISCONNECTED;
+
             //SyncUpdate();
             // 1. 서버 객체 선언
             TcpListener server = new TcpListener(IPAddress.Any, 12345);
@@ -37,20 +54,148 @@ namespace TCPServer
             int byteRead;
 
             // 클라이언트가 보낸 데이터를 계속 읽음
+            // <Unity 클라이언트가 보낸 데이터 형식>
+            // 1. PLC 연결: "Connect" -> MxComponent.Open()
+            // 2. PLC 연결해지: "DisConnect" -> MxComponent.Close()
+            // 3. 데이터 요청: "Request,read,X0,2" -> MxComponent.ReadDeviceBlock(X0, 2, out data[0]);
+            //                 "Request,write,X10,1" -> MxComponent.ReadDeviceBlock(X10, 1, out data[0]);
             while((byteRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
             {
+                // "Connect", "DisConnect", "Request,read,X0,2"
                 string dataStr = Encoding.UTF8.GetString(buffer, 0, byteRead);
                 Console.WriteLine($"수신: {dataStr}");
 
-                // 데이터 받음을 클라이언트에게 알려줌
-                string newData = $"서버 데이터 송신완료";
-                byte[] responseData = Encoding.UTF8.GetBytes(newData);
+                // FSM(finite state machine): 유한상태머신
+                string result = FSM(dataStr); // result -> Unity
+
+                byte[] responseData = Encoding.UTF8.GetBytes(result);
 
                 await stream.WriteAsync(responseData, 0, responseData.Length);
-                Console.WriteLine($"송신: {newData}");
+                Console.WriteLine($"송신: {result}");
             }
         }
 
+        // "Connect", "DisConnect", "Request,read,X0,2"
+        private static string FSM(string dataStr)
+        {
+            if(dataStr.Contains("Connect"))
+            {
+                return Connect();
+            }
+            else if(dataStr.Contains("Disconnect"))
+            {
+                return Disconnect();
+            }
+            else if (dataStr.Contains("Request,read"))
+            {
+                return ReadDeviceBlock(dataStr);
+            }
+            else if(dataStr.Contains("Request,write"))
+            {
+                return WriteDeviceBlock(dataStr);
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
+
+        // 콘솔프로그램 종료시 이벤트
+        static void ExitHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("프로그램 종료 중...");
+
+            Disconnect();
+        }
+
+        private static string WriteDeviceBlock(string dataStr)
+        {
+            // 문자열 파싱: "Request,write,X10,2,355,366" -> { Request, read, X10, 2, 355, 366 }
+            // 시작버튼, 정지버튼, 긴급정지버튼 -> 7(111)
+            string[] data = dataStr.Split(",");
+            string address = data[2];
+            int blockCnt;
+            bool isInt = int.TryParse(data[3], out blockCnt);
+
+            if (!isInt)
+                return "Request Error: BlockCnt 문자열 오류";
+
+            int value;
+            isInt = int.TryParse(data[4], out value);
+
+            if (!isInt)
+                return "Request Error: Device Value 문자열 오류";
+
+            int[] values = new int[blockCnt];
+            values[0] = value;
+
+            int iRet = mxComponent.WriteDeviceBlock(address, blockCnt, ref values[0]);
+            if (iRet == 0)
+            {
+                return $"Data written: {address}/{blockCnt}/{value}";
+            }
+            else
+            {
+                return "0x" + Convert.ToString(iRet, 16);
+            }
+        }
+
+        private static string ReadDeviceBlock(string dataStr)
+        {
+            // 문자열 파싱: "Request,read,X0,1" -> { Request, read, X0, 1 }
+            string[] data = dataStr.Split(",");
+            string address = data[2];
+            int blockCnt;
+            bool isInt = int.TryParse(data[3], out blockCnt);
+
+            if (!isInt)
+                return "Request Error: BlockCnt 문자열 오류";
+
+            int[] newData = new int[blockCnt];
+            int iRet = mxComponent.ReadDeviceBlock(address, blockCnt, out newData[0]);
+            if (iRet == 0)
+            {
+                string str = "";
+                for (int i = 0; i < newData.Length; i++)
+                {
+                    str += newData[i] + ", ";
+                }
+
+                return $"Read,{address},{blockCnt},{str}"; // "Read,X0,1,35" ex) 센서정보들
+            }
+            else
+            {
+                return "0x" + Convert.ToString(iRet, 16);
+            }
+        }
+
+        private static string Disconnect()
+        {
+            int iRet = mxComponent.Close();
+            if (iRet == 0)
+            {
+                state = State.DISCONNECTED;
+                return "Disconnected";
+            }
+            else
+            {
+                return "0x" + Convert.ToString(iRet, 16);
+            }
+        }
+
+        private static string Connect()
+        {
+            int iRet = mxComponent.Open();
+            if (iRet == 0)
+            {
+                state = State.CONNECTED;
+                return "Connected";
+            }
+            else
+            {
+                return "0x" + Convert.ToString(iRet, 16);
+            }
+        }
 
         private static void SyncUpdate()
         {
--- TCPServer/TCPServer/obj/Debug/net9.0/TCPServer.AssemblyInfo.cs
+++ TCPServer/TCPServer/obj/Debug/net9.0/TCPServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646962a8ca19b304f4ef1786edd841e3ad0e8094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0d459a35593a6bee4e009c5930e9f26ef36d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
